43cc99834e79837bed518bc0d709fd82e1edbeb1
divdivpm <divdivpm@gmail.com>
2024-05-02 10:42:02 +0200

    Исправляет ошибки: переименовывает  presenter, consts; убирает обращение к DOM в presenter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import MainPresenter from './presenter/main-presenter.js';
+import TripInfoView from './view/trip-info-view';
+import SortView from './view/sort-view';
+import FilterView from './view/filter-view';
+import EventPresenter from './presenter/event-presenter.js';
+import { render, RenderPosition } from './render';
 
-const sitePageBodyElement = document.querySelector('.page-body');
-const siteTripEventsElement = sitePageBodyElement.querySelector('.trip-events');
+const pageBodyElement = document.querySelector('.page-body');
+const tripMainElement = pageBodyElement.querySelector('.trip-main');
+const controlsFiltersElemet = tripMainElement.querySelector('.trip-controls__filters');
+const tripEventsElement = pageBodyElement.querySelector('.trip-events');
 
-const mainPresenter = new MainPresenter({container: siteTripEventsElement});
+const eventPresenter = new EventPresenter({container: tripEventsElement});
 
-mainPresenter.init();
+eventPresenter.init();
+
+render(new FilterView, controlsFiltersElemet);
+render(new TripInfoView, tripMainElement, RenderPosition.AFTERBEGIN);
+render(new SortView, tripEventsElement);
